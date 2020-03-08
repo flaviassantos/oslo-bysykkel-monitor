8eff5e0f0dd2b4930812f0ab46089d9eaf06a44d
Flavia Santos <flavia.souza.santos@halfwave.com>
2020-03-08 17:56:38 +0100

    Error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+import os
+from logging.handlers import RotatingFileHandler
+import logging
 
 
 app = Flask(__name__)
@@ -27,4 +30,18 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/oslo-bysykkel-monitor.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Oslo Bysykkel Monitor startup')
     return app
---
+++ app/errors/error_handlers.py
@@ -0,0 +1,18 @@
+from flask import render_template, jsonify
+from app.errors import bp
+
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    """
+    Handle pages not found.
+    """
+    return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    """
+    Renders the page for server errors.
+    """
+    return render_template('errors/500.html'), 500
