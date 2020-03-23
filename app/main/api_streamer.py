e1eec5e3d01f577c1a271facbdd768398721bebb
Flavia Santos <flavia.souza.santos@halfwave.com>
2020-03-23 22:44:53 +0100

    Implement API streamer as a class

--- app/main/api_streamer.py
+++ app/main/api_streamer.py
@@ -1,39 +1,83 @@
 import sys
 from flask import flash
-
-import app
 from config import Config
 import requests
 import pandas as pd
 from datetime import datetime, timezone
 
 
-# TODO: transform into a class?
+class StationStreamer:
 
+    def __init__(self):
+        self.headers = {"Client-Identifier": Config.CLIENT_IDENTIFIER}
+        self.columns = ['station_id', 'name', 'num_bikes_available',
+                        'num_docks_available', 'last_reported']
+        self.url_info = Config.URL_INFO
+        self.url_status = Config.URL_STATUS
 
-def fetch_api_data(url=None, headers=None):
-    """
-    Streams data from Oslo Bysykkel open API into a json format.
-    Selects the most appropriate attributes of the station data
-    ('data', 'station' and 'last_updated').
+    def fetch_api_data(self, url=None):
+        """
+        Streams data from Oslo Bysykkel open API into a json format.
+        Selects the most appropriate attributes of the station data
+        ('data', 'station' and 'last_updated').
 
-    Parameters
-    ----------
-    url = string with the url to request from
-    headers =  dictionary with the client identifier required
+        Parameters
+        ----------
+        url = string with the url to request from
+        headers =  dictionary with the client identifier required
 
-    Returns
-    -------
-    response_body['data']['stations']: json data
-    last_updated: integer representing the elapsed time in number of seconds
+        Returns
+        -------
+        response_body['data']['stations']: json data
+        last_updated: integer representing the elapsed time in number of seconds
 
-    """
-    try:
-        response = requests.get(url=url, headers=headers)
-        response_body = response.json()
-        return response_body
-    except requests.exceptions.RequestException as e:
-        raise e
+        """
+        try:
+            response = requests.get(url, self.headers)
+            response_body = response.json()
+            return response_body
+        except requests.exceptions.RequestException as e:
+            raise e
+
+    def get_station_data(self):
+        """
+        Reads data into json format and parse it into a pandas.DataFrame.
+        Keeps only the most relevant attributes of the station data.
+
+        Parameters
+        ----------
+        config_class
+
+        Returns
+        -------
+        station_data: list which contains the station data in a dictionary format
+        last_updated: string with formatted timestamp
+
+        """
+        try:
+            # Station information
+            data_info = self.fetch_api_data(self.url_info)  # json
+            dict_info = data_info['data']['stations']
+
+            # Station status
+            data_status = self.fetch_api_data(self.url_status)  # json
+            dict_status = data_status['data']['stations']
+            last_updated = data_status['last_updated']
+            last_updated = parse_datetime(last_updated)
+
+            # Parse into dataframe and merge by 'station_id'
+            df_status = pd.DataFrame(dict_status)
+            df_info = pd.DataFrame(dict_info)
+            df = pd.concat([df_info, df_status], sort=False, axis=1, join='outer')
+            df = df[df['is_installed'] == 1]
+            df = df[self.columns]
+            df = df.loc[:, ~df.columns.duplicated()]
+            df.sort_values(by='name', inplace=True)
+            station_data = list(df.T.to_dict().values())
+            return station_data, last_updated
+
+        except:
+            flash(f"Oops! Exception: '{sys.exc_info()[0]}' occurred.")
 
 
 def parse_datetime(date_time=None):
@@ -50,48 +94,3 @@ def parse_datetime(date_time=None):
     """
     dt = datetime.fromtimestamp(date_time, timezone.utc).strftime("%B %d, %Y %I:%M:%S")
     return dt
-
-
-def get_station_data(config_class=Config):
-    """
-    Reads data into json format and parse it into a pandas.DataFrame.
-    Keeps only the most relevant attributes of the station data.
-
-    Parameters
-    ----------
-    config_class
-
-    Returns
-    -------
-    station_data: list which contains the station data in a dictionary format
-    last_updated: string with formatted timestamp
-
-    """
-    try:
-        columns = ['station_id', 'name', 'num_bikes_available',
-                   'num_docks_available', 'last_reported']
-        headers = {"Client-Identifier": config_class.CLIENT_IDENTIFIER}
-
-        # Station information
-        data_info = fetch_api_data(url=config_class.URL_INFO, headers=headers)  # json
-        dict_info = data_info['data']['stations']
-
-        # Station status
-        data_status = fetch_api_data(url=config_class.URL_STATUS, headers=headers)  # json
-        dict_status = data_status['data']['stations']
-        last_updated = data_status['last_updated']
-        last_updated = parse_datetime(last_updated)
-
-        # Parse into dataframe and merge by 'station_id'
-        df_status = pd.DataFrame(dict_status)
-        df_info = pd.DataFrame(dict_info)
-        df = pd.concat([df_info, df_status], sort=False, axis=1, join='outer')
-        df = df[df['is_installed'] == 1]
-        df = df[columns]
-        df = df.loc[:, ~df.columns.duplicated()]
-        df.sort_values(by='name', inplace=True)
-        station_data = list(df.T.to_dict().values())
-        return station_data, last_updated
-
-    except:
-        flash(f"Oops! Exception: '{sys.exc_info()[0]}' occurred.")
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from werkzeug.utils import redirect
 from flask import render_template, flash
 from app.main import bp
-from app.main.api_streamer import get_station_data
+from app.main.api_streamer import StationStreamer
 
 
 @bp.route('/')
@@ -11,7 +11,8 @@ def index():
     Renders the home page of the application.
     """
     try:
-        stations, last_updated = get_station_data()
+        api = StationStreamer()
+        stations, last_updated = api.get_station_data()
         return render_template('index.html', stations=stations, last_updated=last_updated)
     except:
         flash("Not possible to retrieve data from the API. "
