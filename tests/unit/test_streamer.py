adf87121c009fd2ef2ee767c5fc92dfb6a5025c4
Flavia Santos <flavia.souza.santos@halfwave.com>
2020-03-23 22:45:30 +0100

    Fix tests for the API streamer

--- tests/unit/test_streamer.py
+++ tests/unit/test_streamer.py
@@ -1,28 +1,29 @@
+import pytest
 import requests
-from app.main.api_streamer import fetch_api_data, parse_datetime, get_station_data
-
-URL_STATUS = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
-URL_INFO = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
-CLIENT_IDENTIFIER = "flavia-oslobysykkelmonitor"
-HEADERS = {"Client-Identifier": CLIENT_IDENTIFIER}
+from app.main.api_streamer import StationStreamer, parse_datetime
 
 
-class TestFetchAPIData:
-    def test_api_response_status(self):
-        resp_status = requests.get(URL_STATUS, headers=HEADERS)
-        resp_info = requests.get(URL_INFO, headers=HEADERS)
-        assert resp_status.status_code == 200
-        assert resp_info.status_code == 200
+@pytest.fixture(scope='module')
+def streamer():
+    return StationStreamer()
 
-    def test_api_response_body(self):
-        rep_status_body = fetch_api_data(URL_STATUS)
-        resp_info_body = fetch_api_data(URL_INFO)
 
-        assert "data" in rep_status_body
-        assert "stations" in rep_status_body["data"]
+def test_api_response_status(streamer):
+    resp_status = requests.get(streamer.url_status, headers=streamer.headers)
+    resp_info = requests.get(streamer.url_info, headers=streamer.headers)
+    assert resp_status.status_code == 200
+    assert resp_info.status_code == 200
 
-        assert "data" in resp_info_body
-        assert "stations" in resp_info_body["data"]
+
+def test_api_response_body(streamer):
+    rep_status_body = streamer.fetch_api_data(streamer.url_status)
+    resp_info_body = streamer.fetch_api_data(streamer.url_info)
+
+    assert "data" in rep_status_body
+    assert "stations" in rep_status_body["data"]
+
+    assert "data" in resp_info_body
+    assert "stations" in resp_info_body["data"]
 
 
 def test_parse_datetime():
@@ -30,14 +31,3 @@ def test_parse_datetime():
     expected = 'March 22, 2020 07:37:57'
     actual = parse_datetime(last_updated)
     assert actual == expected
-
-
-def test_get_station_data():
-    expected_attributes = ['station_id', 'name', 'num_bikes_available', 'num_docks_available', 'last_reported']
-
-    station_data, last_updated = get_station_data()
-    actual_attributes = [k for k, v in station_data[0].items()]
-
-    assert actual_attributes == expected_attributes
-    assert len(station_data) > 0
-    assert type(last_updated) == str
